48368eac0116f80d613350f814ecd1541f122388
BredikhinBoris <boris00043@yandex.ru>
2019-03-21 17:28:55 +0400

    Рефакторинг

--- admin/data/questions/processAnswer.php
+++ admin/data/questions/processAnswer.php
@@ -1,25 +1,38 @@
 <?php
-require_once "../../../config.php";
-session_start();
-include "../../saveUserData.module";
-if (!defined($GLOBALS["sitedata"]))//Сервер тольько что запущен и данные сайта не считаны
-    include "../sitedataParser.php";
+	require_once "../../../config.php";
+	session_start();
+	include "../../saveUserData.module";
+	if (!defined($GLOBALS["sitedata"]))
+		include "../sitedataParser.php";
+	function rightAnswer()
+	{
+		?>
+        <span class='right-answer'>Верно</span>
+        <div id='delta' hidden>1</div>
+		<?php
+		$_SESSION["userdata"]->{"points"}++;
+	}
+	
+	function wrongAnswer()
+	{
+		?>
+        <span class='wrong-answer'>Неверно</span>
+        <div id='delta' hidden>0</div>
+		<?php
+	}
 
 // radio task
 //var_dump($_SESSION);
 //var_dump($_POST);
-$question = $GLOBALS["sitedata"]->{"questions"}[(int)$_POST["id"]];
+	$question = $GLOBALS["sitedata"]->{"questions"}[(int)$_POST["id"]];
 //var_dump($question);
-if ($question->{"type"} == "radio task"){
-    if ($_POST["answer"] == $question->{"rightAnswer"}){
-        echo "<span class='right-answer'>Верно</span><div id = 'delta' hidden>1</div>";
-        $_SESSION["userdata"]->{"points"}++;
-
-    }
-    else{
-        echo "<span class='wrong-answer'>Неверно</span><div id = 'delta' hidden>0</div>";
-    }
-}
-array_push($_SESSION["userdata"]->{'answeredQuestions'}, $_POST["id"]);
-save_user_data();
+	if ($question->{"type"} == "radio task") {
+		if ($_POST["answer"] == $question->{"rightAnswer"}) {
+			rightAnswer();
+		} else {
+			wrongAnswer();
+		}
+	}
+	array_push($_SESSION["userdata"]->{'answeredQuestions'}, $_POST["id"]);
+	save_user_data();
 ?>
