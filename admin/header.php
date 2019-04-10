6ce211df789aa034c407ba2b9d4816be2469e7a6
BredikhinBoris <boris00043@yandex.ru>
2019-04-10 16:10:44 +0400

    Добавлена аналитика

--- admin/header.php
+++ admin/header.php
@@ -8,6 +8,23 @@ if (!defined($GLOBALS["sitedata"]))//Сервер тольько что запу
 <!doctype html>
 <html lang="ru">
 <head>
+    
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(53218108, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true
+        });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/53218108" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
+   
     <title><?=$GLOBALS["sitedata"]->{"name"}." : ".$pageName;?></title>
     <link rel="stylesheet" href=<?="\"".ROOT_SERVER."/css/main.css\""?>>
     <script src="http://code.jquery.com/jquery-3.3.1.js"></script>
